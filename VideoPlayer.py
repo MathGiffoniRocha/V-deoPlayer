5114f327244c162e45b405eb542329f2f7e4e7b1
Matheus Giffoni Rocha <m.rocha2@outlook.com>
2022-02-05 13:29:49 -0300

    Fourth commit

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -5,19 +5,19 @@ import pyautogui
 
 
 
-#Definindo navegadorÇ
+#Defining Navigator --- In case you don't use chrome you must change the webdriver, but before it you may install the one you want and put it in the same folder of the python script
 navegador=webdriver.Chrome()
  
 
-#Abrindo navegador
+#Opening navigator --- Ps.: In the () down here, please make sure you put the page YOU want to open, otherwise you will hear lofi :)
 navegador.get("https://www.youtube.com/c/LofiGirl")
 
 
-#Acessando o vídeo:
+#Acessing the video --- Depending of the position you want to click you may change the coordinates, in order to do it take a look at GetPosition.py in my repository 
 pyautogui.click(x=344, y=619)
 
 
-#Abrindo o vs code:
+#Opening Vs code --- You may change the words in () on line 23 so you can open other stuff
 time.sleep(2)
 pyautogui.press("win")
 pyautogui.write("Visual Studio Code")
